130958149aa02fd7dce354432bad32a7f1d3b0bf
Mauricio MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2024-06-02 09:55:49 -0300

    Refactor to reduce complexity of computing maximum and minimums

--- src/main/java/Matrix.java
+++ src/main/java/Matrix.java
@@ -3,11 +3,11 @@ import java.util.stream.Collectors;
 
 class Matrix {
 
-    private record pointsOfInterest (int value, Set<MatrixCoordinate> elements) {}
+    private record PointsOfInterest(int value, Set<MatrixCoordinate> elements) {}
 
     private final List<List<Integer>> matrix;
-    private final Map<Integer, pointsOfInterest> rowMax = new HashMap<>();
-    private final Map<Integer, pointsOfInterest> colMin = new HashMap<>();
+    private final Map<Integer, PointsOfInterest> rowMax = new HashMap<>();
+    private final Map<Integer, PointsOfInterest> colMin = new HashMap<>();
 
 
     Matrix(List<List<Integer>> values) {
@@ -15,43 +15,50 @@ class Matrix {
     }
 
     Set<MatrixCoordinate> getSaddlePoints() {
-        getRowMaxAndColMin();
-        var maximumPoints = rowMax.values().stream().flatMap(p -> p.elements.stream()).collect(Collectors.toSet());
-        var minimumPoints = colMin.values().stream().flatMap(p -> p.elements.stream()).collect(Collectors.toSet());
-        return maximumPoints.stream().filter(minimumPoints::contains).collect(Collectors.toSet());
+        computeRowMaxAndColMin();
+        return getPointsOfInterest(rowMax).stream()
+                .filter(getPointsOfInterest(colMin)::contains)
+                .collect(Collectors.toSet());
     }
 
-    private void getRowMaxAndColMin() {
+    private void computeRowMaxAndColMin() {
         for (int rowIndex = 0; rowIndex < matrix.size(); rowIndex++) {
             var row = matrix.get(rowIndex);
             for (int colIndex = 0; colIndex < row.size(); colIndex++) {
-                int value = row.get(colIndex);
-                var coordinate = new MatrixCoordinate(rowIndex + 1, colIndex + 1);
-                checkRowMax(rowIndex, value, coordinate);
-                checkColMin(colIndex, value, coordinate);
+                updateColAndRow(row, colIndex, rowIndex);
             }
         }
     }
 
-    private void checkColMin(int colIndex, Integer value, MatrixCoordinate coordinate) {
-        var min = colMin.getOrDefault(colIndex, new pointsOfInterest(value, new HashSet<>()));
-        if (value < min.value) {
-            min = new pointsOfInterest(value, new HashSet<>(List.of(coordinate)));
-        } else if (value == min.value) {
-            min.elements.add(coordinate);
-        }
+    private Set<MatrixCoordinate> getPointsOfInterest(Map<Integer, PointsOfInterest> rowMax) {
+        return rowMax.values()
+                .stream()
+                .flatMap(p -> p.elements.stream())
+                .collect(Collectors.toSet());
+    }
+
+    private void updateColAndRow(List<Integer> row, int colIndex, int rowIndex) {
+        int value = row.get(colIndex);
+        var coordinate = new MatrixCoordinate(rowIndex + 1, colIndex + 1);
+        updateColMin(colIndex, value, coordinate);
+        updateRowMax(rowIndex, value, coordinate);
+    }
+
+    private void updateColMin(int colIndex, int value, MatrixCoordinate coordinate) {
+        var min = colMin.get(colIndex);
+        if (min == null || value < min.value) min = new PointsOfInterest(value, new HashSet<>());
+        if (value == min.value) min.elements.add(coordinate);
         colMin.put(colIndex, min);
     }
 
-    private void checkRowMax(int rowIndex, Integer value, MatrixCoordinate coordinate) {
-        var max = rowMax.getOrDefault(rowIndex, new pointsOfInterest(value, new HashSet<>()));
-        if (value > max.value) {
-            max = new pointsOfInterest(value, new HashSet<>(List.of(coordinate)));
-        } else if (value == max.value) {
-            max.elements.add(coordinate);
-        }
+    private void updateRowMax(int rowIndex, int value, MatrixCoordinate coordinate) {
+        var max = rowMax.get(rowIndex);
+        if (max == null || value > max.value) max = new PointsOfInterest(value, new HashSet<>());
+        if (value == max.value) max.elements.add(coordinate);
         rowMax.put(rowIndex, max);
     }
+
+
 
 
 }
